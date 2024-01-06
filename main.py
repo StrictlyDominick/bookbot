4f06937f5e8d736eda4668510199dbe72b603fdb
StrictlyDominick <strictlydominick@gmail.com>
2024-01-06 17:37:05 -0500

    added get_unique_chars() and added count_all_chars()

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def main():
+    book = get_book_contents("books/frankenstein.txt")
+    print(count_all_chars(book))
+
 def get_book_contents(file_path):
     with open(file_path) as f:
         file_contents = f.read()
@@ -6,10 +10,21 @@ def get_book_contents(file_path):
 def count_words(txt):
     return len(txt.split())
 
-def main():
-    book_content = get_book_contents("books/frankenstein.txt")
-    words_counted = count_words(book_content)
+def get_unique_chars(txt):
+    char_list = []
+    for i in range(0, len(txt)):
+        if char_list.count(txt[i]) == 0:
+            char_list.append(txt[i])
+    return char_list
 
-    return print(words_counted)
+def count_all_chars(txt):
+    char_counts = {}
+    lowercase_txt = txt.lower()
+    char_list = get_unique_chars(lowercase_txt)
+
+    for char in char_list:
+        char_counts[char] = lowercase_txt.count(char)
+
+    return char_counts
 
 main()
