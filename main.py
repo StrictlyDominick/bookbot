752910d0b732b1051f398e3d559d26fef31101a2
StrictlyDominick <strictlydominick@gmail.com>
2024-01-06 18:04:31 -0500

    implemented print_book_report()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
-    book = get_book_contents("books/frankenstein.txt")
-    print(count_all_chars(book))
+    book_path = "books/frankenstein.txt"
+    print_book_report(book_path)
 
 def get_book_contents(file_path):
     with open(file_path) as f:
@@ -26,5 +26,22 @@ def count_all_chars(txt):
         char_counts[char] = lowercase_txt.count(char)
 
     return char_counts
+
+def print_book_report(book_path):
+    print(f"--- Begin report of {book_path} ---")
+    book_contents = get_book_contents(book_path)
+
+    print(f"{count_words(book_contents)} words found in the document")
+    print("")
+    
+    dict_of_char_counts = count_all_chars(book_contents)
+    dict_list_keys = list(dict_of_char_counts.keys())
+    dict_list_keys.sort()
+    
+    for key in dict_list_keys:
+        if key.isalpha():
+            print(f"The '{key}' character was found {dict_of_char_counts[key]} times.")
+
+    print("--- End report ---")
 
 main()
